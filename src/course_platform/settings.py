70e3583a25cd505b3fb41b58ed43177fb02f3d48
Bipana Bam <bipanabam147@gmail.com>
2025-03-22 22:56:47 +0545

    Setting Email and EmailVerificationEvent model in new emails app

--- src/course_platform/settings.py
+++ src/course_platform/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+# default Backend
+EMAIL_HOST = config("EMAIL_HOST", cast=str, default=None)
+EMAIL_PORT = config("EMAIL_PORT", cast=str, default='587')
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str, default=None)
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str, default=None)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=str, default=True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
+    'emails',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +139,6 @@ MEDIA_ROOT = LOCAL_CDN / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CLoudinary
-from decouple import config
 
 CLOUDINARY_CLOUD_NAME = config("CLOUDINARY_CLOUD_NAME", default="")
 CLOUDINARY_PUBLIC_API_KEY = config("CLOUDINARY_PUBLIC_API_KEY", default="")
